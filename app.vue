497f479bfac6516353934fa9b32a7350d4690aae
Michael Kryvoruchko <mk01@rpsdev.ch>
2024-02-20 17:01:18 +0200

    temp

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <VApp>
+      <VAppBar
+        app
+        flat
+        color="primary"
+      >
+        <VToolbarTitle>App</VToolbarTitle>
+      </VAppBar>
+
+      <VMain>
+        <VContainer>
+          <NuxtPage />
+        </VContainer>
+      </VMain>
+    </VApp>
   </NuxtLayout>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,11 @@
+<script setup lang="ts">
+import type { ISection } from '~/types'
+
+const { data } = await useFetch<any>('/api/get-data')
+</script>
+
 <template>
-  <h3>Welcome to your <b>Nuxt.js app</b>!</h3>
+  <div class="bg-gray-100 p-5">
+    {{ data }}
+  </div>
 </template>
-
-<style>
-body {
-  @apply m-2 mx-4
-}
-
-
-
-
-h3 { @apply font-sans text-sm font-medium }
-
-b {
-  @apply text-emerald-400 font-sans text-sm
-}
-</style>
